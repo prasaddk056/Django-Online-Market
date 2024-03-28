927ea6ef5f3732cfa6d33410e891223ae618b5a9
prasaddk056 <prasaddk056@gmail.com>
2024-03-28 14:54:29 +0530

    changed core:urls logout

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path("contact/", views.contact , name = "contact"),
     path("signup/",views.signup , name = "signup"),
     path("login/",auth_views.LoginView.as_view(template_name = "core/login.html",authentication_form = LoginForm) , name = "login"),
-    path("logout0" , views.logout_user , name="logout")
+    path("logout/" , views.logout_user , name="logout")
 ]
 
